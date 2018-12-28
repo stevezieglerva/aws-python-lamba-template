6f3de0e347ee9b18ec95008d66385951c4b0e7c4
Ziegler <18589@icf.com>
2018-12-28 09:08:06 -0500

    Removed specific code

--- lambda_function.py
+++ lambda_function.py
@@ -16,42 +16,6 @@ def lambda_handler(event, context):
 			aws_request_id = context.aws_request_id
 
 		print("Started")
-		if "text_logging" in os.environ:
-			log = structlog.get_logger()
-		else:
-			log = setup_logging("aws-code-index-stream-bulk-load", event, aws_request_id)
-
-
-		s3 = boto3.resource("s3")
-		dynamodb_urls_to_process = []
-		for event in event["Records"]:
-			if event["eventName"] == "INSERT":
-				s3_url = event["dynamodb"]["Keys"]["s3-url"]["S"]
-				print("Found: " + s3_url)
-				dynamodb_urls_to_process.append(s3_url)
-
-		if len(dynamodb_urls_to_process) > 0:
-			file_texts = get_file_text_from_s3_urls(dynamodb_urls_to_process, s3)
-			combined_text = ""
-			for file in file_texts:
-				combined_text = combined_text + "\n____\n" + file + "\n" + file_texts[file] 
-			print("combined_text length: " + str(len(combined_text)))
-			base_filename = "combined-files/" + datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S") + "_" + aws_request_id
-			combined_file_name = base_filename + ".txt"
-			print("combined file name:" + combined_file_name)
-			create_s3_text_file("code-index", combined_file_name, combined_text, s3)
-			create_csv_file(base_filename, file_texts, s3)
-
-			e = Event("", "")
-			for file in dynamodb_urls_to_process:
-				e.purge_event("code-index", file)
-			log.critical("process_results", input_file_count=len(dynamodb_urls_to_process), processed_file_count=len(file_texts))
-			result = {"msg" : "Success", "input_file_count" : len(dynamodb_urls_to_process), "processed_file_count" : len(file_texts)}
-			log.critical("finished", result=result)
-		else:
-			print("No INSERT events")
-			result = {"msg" : "Success - No INSERT events", "file_count" : 0}
-			log.critical("finished_no_insert_events", result=result)
 
 		print("Finished")
 
@@ -59,7 +23,7 @@ def lambda_handler(event, context):
 		print("Exception: "+ str(e))
 		raise(e)
 
-	return result
+	return {"msg" : "Success"}
 
 
 
@@ -93,16 +57,3 @@ def setup_logging(lambda_name, lambda_event, aws_request_id):
 	log.critical("started", input_events=json.dumps(lambda_event, indent=3))
 
 	return log
-
-
-def create_csv_file(base_filename, file_texts, s3):
-	csv = create_csv_file_contents(file_texts)
-	create_s3_text_file("code-index", base_filename + ".csv", csv, s3)
-
-
-def create_csv_file_contents(file_texts):
-	csv = "filename,file_text"
-	row_template = "\"{}\",\"{}\""
-	for file in file_texts:
-		csv = csv + "\n" + row_template.format(file, file_texts[file])
-	return csv
